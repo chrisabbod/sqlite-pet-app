48a0e6434197bd2b3fcca279b3198b2a8ec2b3a4
Chris <chrisabbod@gmail.com>
2018-07-17 23:46:05 -0400

    <feat>: Create PetContract Class

---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,8 @@
+package com.example.android.pets.data;
+
+//Class made final so it can't be extended. This is because this is just a class for providing
+//constants and we won't need to extend or implement anything for its outer class
+
+public final class PetContract {
+
+}
