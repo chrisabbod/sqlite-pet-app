e49cf4294948dbdc3cac1e46cb6e75a82f583bb8
Chris <chrisabbod@gmail.com>
2018-08-13 00:04:10 -0400

    <feat>: Updated Remaining Functions to Use URIs and ContentProviders

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -19,6 +19,7 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -68,8 +69,6 @@ public class CatalogActivity extends AppCompatActivity {
      * the pets database.
      */
     private void displayDatabaseInfo(){
-        //Create and/or open a database to read from it
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
         String [] projection = {
                 PetEntry._ID,
@@ -79,14 +78,23 @@ public class CatalogActivity extends AppCompatActivity {
                 PetEntry.COLUMN_PET_WEIGHT
         };
 
-        Cursor cursor = db.query(
-            PetEntry.TABLE_NAME,
-            projection,
-            null,
-            null,
-            null,
-            null,
-            null);
+//        Cursor cursor = db.query(
+//            PetEntry.TABLE_NAME,
+//            projection,
+//            null,
+//            null,
+//            null,
+//            null,
+//            null);
+
+        //Perform a query on the provider using the ContentResolver.
+        //Use the CONTENT_URI to access the pet data
+        Cursor cursor = getContentResolver().query(
+                PetEntry.CONTENT_URI,   //The content URI of the words table
+                projection,             //The columns to return for each row
+                null,          //Selection criteria
+                null,       //Selection criteria
+                null);        //The sort order for the returned data
 
         TextView displayView = (TextView)findViewById(R.id.text_view_pet);
 
@@ -134,20 +142,20 @@ public class CatalogActivity extends AppCompatActivity {
         }
     }
 
-    private void insertPet(){
-        //Gets the data repository in write mode
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
-
+    private void insertPet() {
+        // Create a ContentValues object where column names are the keys,
+        // and Toto's pet attributes are the values.
         ContentValues values = new ContentValues();
-
         values.put(PetEntry.COLUMN_PET_NAME, "Toto");
         values.put(PetEntry.COLUMN_PET_BREED, "Terrier");
         values.put(PetEntry.COLUMN_PET_GENDER, PetEntry.GENDER_MALE);
         values.put(PetEntry.COLUMN_PET_WEIGHT, 7);
 
-        long newRowId = db.insert(PetEntry.TABLE_NAME, null, values);
-
-        Log.v("CatalogActivity", "New Row ID " + newRowId);
+        // Insert a new row for Toto into the provider using the ContentResolver.
+        // Use the {@link PetEntry#CONTENT_URI} to indicate that we want to insert
+        // into the pets database table.
+        // Receive the new content URI that will allow us to access Toto's data in the future.
+        Uri newUri = getContentResolver().insert(PetEntry.CONTENT_URI, values);
     }
 
     @Override
